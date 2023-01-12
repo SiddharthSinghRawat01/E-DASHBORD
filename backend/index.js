fdbebb2bf92513dc0fc653f5f16304eec0b89293
SiddharthSinghRawat01 <Siddharths.tas@gmail.com>
2023-01-12 13:58:09 +0530

    login sign up

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,28 @@ app.use(cors());
 app.post('/register', async (req,res)=>{
     let user = await new User(req.body);
     let result = await user.save();
+    result= result.toObject();
+    delete result.password;
     res.send(result)
 })
+
+app.post("/login",async (req,res)=>{
+    console.log(req.body);
+    if(req.body.password && req.body.email){
+        let user = await User.findOne(req.body).select('-password');
+    if(user){
+        res.send(user);
+    } else {
+        res.send({msg:"no user found"})
+    }
+    } else {
+        res.send({msg:"no user found"})
+    }
+    
+    
+});
+
+
 
 
 app.listen(5000,(err)=>{
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = ()=>{
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const auth = localStorage.getItem('user');
+        if(auth){
+            navigate('/')
+        }
+    })
+
+    const handelLogin = async ()=>{
+        console.log(email,password);
+        let result = await fetch('http://localhost:5000/login',{
+            method:'post',
+            body: JSON.stringify({email,password}),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result);
+        if(result.name){
+    localStorage.setItem("user",JSON.stringify(result));
+    navigate("/");
+        }else{
+            alert("please enter correct details");
+        }
+
+    }
+
+    return(
+        <div className='login'>
+            <input type="email" value={email} className='inputBox' placeholder='Enter Email' onChange={(e)=> setEmail(e.target.value)} />
+            <input type="password" value={password} className='inputBox' placeholder='Enter Password' onChange={(e)=> setPassword(e.target.value)} />
+            <button onClick={handelLogin} className='appButton' type='button'>Log In</button>
+        </div>
+    )
+}
+
+export default Login;
