9adf827285419e7eea107d26d6fb737ae43b438e
SiddharthSinghRawat01 <Siddharths.tas@gmail.com>
2023-01-13 12:26:31 +0530

    update complition

--- frontend/src/components/UpdateProduct.js
+++ frontend/src/components/UpdateProduct.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const UpdateProduct = ()=>{
 
@@ -7,11 +7,37 @@ const UpdateProduct = ()=>{
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
-    const [error,setError] = useState(false)
-    const navigate =  useNavigate();
+    const [error,setError] = useState(false);
+    const params = useParams();
+    const navigate = useNavigate()
+;
+    useEffect(()=>{
+        getProductDetails();
+    },[])
+
+    const getProductDetails = async ()=>{
+        // console.warn(params);
+        let result = await fetch(`http://localhost:5000/product/${params.id}`)
+        result = await result.json();
+        console.warn(result);
+        setName(result.name);
+        setPrice(result.price);
+        setCategory(result.category);
+        setCompany(result.company);
+    }
 
     const updateProduct = async ()=>{
         console.warn(name,price,category,company);
+        let result = await fetch(`http://localhost:5000/product/${params.id}`,{
+        method: 'Put',    
+        body: await JSON.stringify({name,price,category,company}),
+        headers:{
+            'Content-Type':"application/json"
+        }
+        });
+        result = await result.json() // data from read stream to jaon
+        console.warn(result);
+        navigate('/')
     }
 
     return (
